2e9682b679ce6bcb6b715ef7bd769c03fb3adb86
Mundoinformatica Canaria <mundoinformaticacanaria@gmail.com>
2026-08-09 23:17:07 +0100

    refactor: add consultation orchestration controller

---
+++ src/application/consultation-controller.js
@@ -0,0 +1,43 @@
+import { ConsultationSession } from "./consultation-session.js";
+
+export class ConsultationController {
+  constructor({ recognizer, synthesizer, sessionFactory = (mode) => new ConsultationSession({ mode }) } = {}) {
+    this.recognizer = recognizer;
+    this.synthesizer = synthesizer;
+    this.sessionFactory = sessionFactory;
+    this.session = null;
+  }
+
+  start(mode = "app") {
+    this.stopVoice();
+    this.session = this.sessionFactory(mode);
+    return this.session.currentQuestion();
+  }
+
+  answer(value) {
+    if (!this.session) throw new Error("Consultation has not started");
+    return this.session.answer(value);
+  }
+
+  speak(text, handlers = {}) {
+    if (!this.synthesizer?.isSupported?.()) return false;
+    this.synthesizer.speak(text, handlers);
+    return true;
+  }
+
+  listen(handlers = {}) {
+    if (!this.recognizer?.isSupported?.()) return false;
+    this.recognizer.listen(handlers);
+    return true;
+  }
+
+  stopVoice() {
+    this.recognizer?.stop?.();
+    this.synthesizer?.stop?.();
+  }
+
+  reset() {
+    this.stopVoice();
+    this.session = null;
+  }
+}
